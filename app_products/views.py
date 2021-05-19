7f2090da222b8445f4e6ded514613e03ecdc1f8e
Priyank Ajudiya <priyank_ajudiya@hotmail.com>
2021-05-19 20:03:02 +0530

    Many updates like category filter releted products

--- app_products/views.py
+++ app_products/views.py
@@ -1,9 +1,12 @@
 from django.http import request
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.views.generic import View, DetailView
+from django.views.generic import View, DetailView, ListView
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from . import models
 from app_cart.models import Cart
+
+from random import shuffle
 # Create your views here.
 
 #################################### all Product Shop
@@ -18,15 +21,13 @@ from app_cart.models import Cart
 #         return render(request, 'app_products/shop.html', context)
 
 ###################################################
-from django.views.generic import ListView
-from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 class shop(ListView):
     model = models.product
     template_name = 'app_products/shop.html'
     context_object_name = "products"
     paginate_by = 4
-    queryset = models.product.objects.all()
+    queryset = models.product.objects.all().order_by('-upload_date')
 
     def get_context_data(self, **kwargs):
         context = super(shop, self).get_context_data(**kwargs)
@@ -41,15 +42,37 @@ class productDetails(DetailView):
     template_name = 'app_products/product-detail.html'
     context_object_name = 'product'
 
+    def get_context_data(self, **kwargs):
+        context = super(productDetails, self).get_context_data(**kwargs)
+
+        relatedProducts = list(models.product.objects.filter(cat__productCategory = self.object.cat).exclude(id=self.object.id))
+        shuffle(relatedProducts)
+        
+        ######## Context ########
+        context['relatedProducts'] = relatedProducts[:2]
+        return context
+
 ################################### Product filter
 class productFilter(View):
     def get(self, request):
         context = {}
         search = request.GET.get('filter')
-        
         products = models.product.objects.filter(cat__productCategory=search)
-            
+
+        page = request.GET.get('page', 1)
+
+        paginator = Paginator(products, 3)
+        try:
+            product = paginator.page(page)
+        except PageNotAnInteger:
+            product = paginator.page(1)
+        except EmptyPage:
+            product = paginator.page(paginator.num_pages)
+
+        ######## Context ########
         context['categorys'] = models.category.objects.all()
-        context['products'] = products
+        context['products'] = product
+        context['categoryName'] = search
+        
 
         return render(request, 'app_products/shop-filter.html', context)
